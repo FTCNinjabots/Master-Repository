9e0f71b75e731ab595167a830dd6a1d111e70c17
Anay Pant <anaypant212@gmail.com>
2020-11-21 21:33:12 -0800

    Aum is not paying attention

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vasu/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vasu/Test.java
@@ -1,4 +1,24 @@
 package org.firstinspires.ftc.teamcode.vasu;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-public class Test {
+import org.firstinspires.ftc.teamcode.anay.vision.OpenCVVision;
+
+@Disabled
+@Autonomous(name="Autonomous", group = "Autonomous")
+
+public class Test extends LinearOpMode {
+    @Override
+
+    OpenCVVision Test1;
+
+    public void runOpMode() {
+        Test1.runOpMode();
+
+        if(Test1.pipeline.RingPosition = OpenCVVision.SkystoneDeterminationPipeline.RingPosition.FOUR){
+
+        }
+
+    }
 }
