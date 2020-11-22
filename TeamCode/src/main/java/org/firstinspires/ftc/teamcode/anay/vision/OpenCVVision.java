92d22d311c1fdf349e6231d85de0fb4fc2ac4108
Anay Pant <anaypant212@gmail.com>
2020-11-21 21:51:05 -0800

    Aum is not paying attention

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/anay/vision/OpenCVVision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/anay/vision/OpenCVVision.java
@@ -39,14 +39,13 @@ import org.openftc.easyopencv.OpenCvPipeline;
 import org.openftc.easyopencv.OpenCvWebcam;
 
 @TeleOp
-public class OpenCVVision extends LinearOpMode
-{
+public class OpenCVVision extends LinearOpMode {
     OpenCvCamera phoneCam;
-    SkystoneDeterminationPipeline pipeline;
+    public SkystoneDeterminationPipeline pipeline;
+    public SkystoneDeterminationPipeline.RingPosition position;
 
     @Override
-    public void runOpMode()
-    {
+    public void runOpMode() {
 
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         phoneCam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "webcam"), cameraMonitorViewId);
@@ -58,35 +57,39 @@ public class OpenCVVision extends LinearOpMode
         // landscape orientation, though.
         phoneCam.setViewportRenderingPolicy(OpenCvCamera.ViewportRenderingPolicy.OPTIMIZE_VIEW);
 
-        phoneCam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
-        {
+        phoneCam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
             @Override
-            public void onOpened()
-            {
-                phoneCam.startStreaming(320,240);
+            public void onOpened() {
+                phoneCam.startStreaming(320, 240);
             }
         });
 
-        waitForStart();
+        //waitForStart();
 
-        while (opModeIsActive())
-        {
-            telemetry.addData("Analysis", pipeline.getAnalysis());
-            telemetry.addData("Position", pipeline.position);
-            telemetry.update();
+
+
+        //This should be commented out
+        //while (opModeIsActive()) {
+         //   telemetry.addData("Analysis", pipeline.getAnalysis());
+         //   telemetry.addData("Position", SkystoneDeterminationPipeline.position);
+         //   telemetry.update();
 
             // Don't burn CPU cycles busy-looping in this sample
-            sleep(50);
-        }
+          //  sleep(50);
+//        }
+
+
+
+
+
+
     }
 
-    public static class SkystoneDeterminationPipeline extends OpenCvPipeline
-    {
+    public static class SkystoneDeterminationPipeline extends OpenCvPipeline {
         /*
          * An enum to define the skystone position
          */
-        public enum RingPosition
-        {
+        public enum RingPosition {
             FOUR,
             ONE,
             NONE
@@ -101,7 +104,7 @@ public class OpenCVVision extends LinearOpMode
         /*
          * The core values which define the location and size of the sample regions
          */
-        static final Point REGION1_TOPLEFT_ANCHOR_POINT = new Point(181,98);
+        static final Point REGION1_TOPLEFT_ANCHOR_POINT = new Point(181, 98);
 
         static final int REGION_WIDTH = 35;
         static final int REGION_HEIGHT = 25;
@@ -125,29 +128,26 @@ public class OpenCVVision extends LinearOpMode
         int avg1;
 
         // Volatile since accessed by OpMode thread w/o synchronization
-        private volatile RingPosition position = RingPosition.FOUR;
+        private static volatile RingPosition position = RingPosition.FOUR;
 
         /*
          * This function takes the RGB frame, converts to YCrCb,
          * and extracts the Cb channel to the 'Cb' variable
          */
-        void inputToCb(Mat input)
-        {
+        void inputToCb(Mat input) {
             Imgproc.cvtColor(input, YCrCb, Imgproc.COLOR_RGB2YCrCb);
             Core.extractChannel(YCrCb, Cb, 1);
         }
 
         @Override
-        public void init(Mat firstFrame)
-        {
+        public void init(Mat firstFrame) {
             inputToCb(firstFrame);
 
             region1_Cb = Cb.submat(new Rect(region1_pointA, region1_pointB));
         }
 
         @Override
-        public Mat processFrame(Mat input)
-        {
+        public Mat processFrame(Mat input) {
             inputToCb(input);
 
             avg1 = (int) Core.mean(region1_Cb).val[0];
@@ -160,11 +160,11 @@ public class OpenCVVision extends LinearOpMode
                     2); // Thickness of the rectangle lines
 
             position = RingPosition.FOUR; // Record our analysis
-            if(avg1 > FOUR_RING_THRESHOLD){
+            if (avg1 > FOUR_RING_THRESHOLD) {
                 position = RingPosition.FOUR;
-            }else if (avg1 > ONE_RING_THRESHOLD){
+            } else if (avg1 > ONE_RING_THRESHOLD) {
                 position = RingPosition.ONE;
-            }else{
+            } else {
                 position = RingPosition.NONE;
             }
 
@@ -178,9 +178,10 @@ public class OpenCVVision extends LinearOpMode
             return input;
         }
 
-        public int getAnalysis()
-        {
+        public int getAnalysis() {
             return avg1;
         }
+
     }
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vasu/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vasu/Test.java
@@ -2,6 +2,8 @@ package org.firstinspires.ftc.teamcode.vasu;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.firstinspires.ftc.teamcode.anay.vision.OpenCVVision;
 
@@ -9,15 +11,30 @@ import org.firstinspires.ftc.teamcode.anay.vision.OpenCVVision;
 @Autonomous(name="Autonomous", group = "Autonomous")
 
 public class Test extends LinearOpMode {
-    @Override
-
     OpenCVVision Test1;
-
+    DcMotor bl = hardwareMap.get(DcMotor.class, "bl");
+    DcMotor br = hardwareMap.get(DcMotor.class, "br");
+    DcMotor fl = hardwareMap.get(DcMotor.class, "fl");
+    DcMotor fr = hardwareMap.get(DcMotor.class, "fr");
+    @Override
     public void runOpMode() {
+
+
+         br.setDirection(DcMotor.Direction.REVERSE);
+         fr.setDirection(DcMotor.Direction.REVERSE);
+
         Test1.runOpMode();
+        if(Test1.position == OpenCVVision.SkystoneDeterminationPipeline.RingPosition.FOUR){
+            telemetry.addData("Position: ", "Four Rings");
+        }
 
-        if(Test1.pipeline.RingPosition = OpenCVVision.SkystoneDeterminationPipeline.RingPosition.FOUR){
+        else if(Test1.position == OpenCVVision.SkystoneDeterminationPipeline.RingPosition.ONE){
+            telemetry.addData("Position: ", "One Ring");
+            telemetry.update();
+        }
 
+        else if(Test1.position == OpenCVVision.SkystoneDeterminationPipeline.RingPosition.NONE){
+            telemetry.addData("Position: ", "No Rings");
         }
 
     }
