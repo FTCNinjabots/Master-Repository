a7795f417688495e51730d581be64ed05fe57794
Maurya <ftcninjabots2020@gmail.com>
2020-12-19 20:22:41 -0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vasu/RT.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vasu/C_Site.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vasu/C_Site.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@Disabled
+
 
 @Autonomous(name="CS", group="CS")
 
@@ -21,42 +21,44 @@ public class C_Site extends LinearOpMode {
 
     public void runOpMode() {
 
-        bl = hardwareMap.get(DcMotor.class, "bl");
-        br = hardwareMap.get(DcMotor.class, "br");
-        fl = hardwareMap.get(DcMotor.class, "fl");
-        fr = hardwareMap.get(DcMotor.class, "fr");
+            bl = hardwareMap.get(DcMotor.class, "bl");
+            br = hardwareMap.get(DcMotor.class, "br");
+            fl = hardwareMap.get(DcMotor.class, "fl");
+            fr = hardwareMap.get(DcMotor.class, "fr");
 
-        bl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        br.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        fl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        fr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            bl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            br.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            fl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            fr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        br.setDirection(DcMotor.Direction.REVERSE);
-        fr.setDirection(DcMotor.Direction.REVERSE);
-        bl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        br.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        fl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        fr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-
-        waitForStart();
+            br.setDirection(DcMotor.Direction.REVERSE);
+            fr.setDirection(DcMotor.Direction.REVERSE);
+            bl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            br.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            fl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            fr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
 
-        while (opModeIsActive()) {
 
-            bl.setTargetPosition(1620);
-            br.setTargetPosition(1620);
-            fl.setTargetPosition(1620);
-            fr.setTargetPosition(1620);
-            bl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            br.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            fl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            fr.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            bl.setPower(0.5);
-            br.setPower(0.5);
-            fl.setPower(0.5);
-            fr.setPower(0.5);
-
+            waitForStart();
+            while (opModeIsActive()) {
+                bl.setTargetPosition(14400);
+                br.setTargetPosition(14400);
+                fl.setTargetPosition(14400);
+                fr.setTargetPosition(14400);
+                bl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                br.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                fl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                fr.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                bl.setPower(0.5);
+                br.setPower(0.5);
+                fl.setPower(0.5);
+                fr.setPower(0.5);
+                float current_encoder = bl.getCurrentPosition();
+                telemetry.addData("Encoder Position: ", current_encoder);
+                telemetry.update();
+            }
+            /*
             sleep(700);
 
             bl.setTargetPosition(-1620);
@@ -70,7 +72,6 @@ public class C_Site extends LinearOpMode {
             bl.setPower(-0.5);
             br.setPower(-0.5);
             fl.setPower(-0.5);
-            fr.setPower(-0.5);
-        }
+            fr.setPower(-0.5);*/
     }
 }
